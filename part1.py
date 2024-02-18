14e8365312fc3d57d4c137c2a441159af8492d4d
zachmixcsu <160365801+zachmixcsu@users.noreply.github.com>
2024-02-17 19:20:07 -0500

    Create assignment one and add part one + two

---
+++ part1.py
@@ -0,0 +1,11 @@
+print("This program will ask for two numbers and then will print the sum and difference of those numbers")
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+
+
+sum = num1 + num2
+difference = num1 - num2
+
+print(f'The sum of the two numbers is {sum}.')
+print(f'The difference of the two numbers is {difference}.')
+
---
+++ part2.py
@@ -0,0 +1,11 @@
+print("This program will ask for two numbers and then will print the product and quotient of those numbers")
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+
+
+product = num1 * num2
+quotient = num1 / num2
+
+print(f'The product of the two numbers is {product}.')
+print(f'The quotient of the two numbers is {quotient}.')
+
